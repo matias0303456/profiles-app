61c1733136e34cd68c1a669b292bb65d6ba7056b
Matias Ramos <matiasramos@gmail.com>
2023-03-28 18:01:24 -0300

    perfil de usuario fixes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -17,19 +17,31 @@ export function Profile() {
         getUsers()
     }, [])
 
+    const profileUser = users.find(item => parseInt(item.id) === parseInt(params.id)) || auth.user
+
+    if (!profileUser) return <h3>Loading...</h3>
+
     return (
         <>
             <h2 className="text-3xl text-center mb-6">
                 {parseInt(params.id) === parseInt(auth.user.id) ?
                     'My profile' :
-                    `${users.find(item => parseInt(item.id) === parseInt(params.id))?.username}'s profile`
+                    `${profileUser.username}'s profile`
                 }
             </h2>
+            <div>
+                <h3>User data</h3>
+                <img src={profileUser.profile.avatar} alt="" />
+                <h4>Email</h4>
+                <p>{profileUser.email}</p>
+                <h4>Bio</h4>
+                <p>{profileUser.profile.bio}</p>
+            </div>
             <div className="flex gap-5">
                 <div>
                     <h3>Followers</h3>
                     <UsersList
-                        users={users.filter(user => auth.user.profile.follows.map(follow => follow.follower.id).includes(user.id))}
+                        users={users.filter(user => profileUser.profile.follows.map(follow => follow.follower.id).includes(user.id))}
                         handleDeleteFollow={handleDeleteFollow}
                         followText="Delete"
                     />
@@ -37,7 +49,7 @@ export function Profile() {
                 <div>
                     <h3>Following</h3>
                     <UsersList
-                        users={users.filter(user => auth.user.follows.map(follow => follow.followed.id).includes(user.profile.id))}
+                        users={users.filter(user => profileUser.follows.map(follow => follow.followed.id).includes(user.profile.id))}
                         handleUnfollow={handleUnfollow}
                         followText="Unfollow"
                     />
