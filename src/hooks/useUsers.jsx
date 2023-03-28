d30ea7dfaf45ba34609b1a5ac3bc6df13a2b4bd2
Matias Ramos <matiasramos@gmail.com>
2023-03-28 16:00:08 -0300

    handle follow

--- src/hooks/useUsers.jsx
+++ src/hooks/useUsers.jsx
@@ -1,28 +1,50 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 
 import { AuthContext } from "../context/AuthProvider";
 import { API_URL } from "../utils/config";
 
 export function useUsers() {
 
-    const { auth } = useContext(AuthContext)
+    const { auth, setAuth } = useContext(AuthContext)
 
     const [users, setUsers] = useState([])
 
-    useEffect(() => {
-        (async () => {
-            const res = await fetch(API_URL + `/user/${auth.user.id}`, {
-                method: 'GET',
+    async function getUsers() {
+        const res = await fetch(API_URL + `/user/${auth?.user.id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': auth?.token
+            }
+        })
+        const data = await res.json()
+        setUsers(data)
+    }
+
+    async function handleFollow(followed) {
+        if (!auth) return
+        try {
+            const res = await fetch(API_URL + `/follow/${auth?.user.id}`, {
+                method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': auth.token
-                }
+                    'Authorization': auth?.token
+                },
+                body: JSON.stringify({ followed })
             })
             const data = await res.json()
-            setUsers(data)
-        })()
-    }, [])
+            setAuth({ ...auth, user: { ...auth.user, follows: [...auth.user.follows, data] } })
+            setUsers([...users.filter(item => item.profile.id !== data.id)])
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
-    return { users }
+    function handleUnfollow() {
+        if (!auth) return
+
+    }
+
+    return { getUsers, users, handleFollow, handleUnfollow }
 
 }
