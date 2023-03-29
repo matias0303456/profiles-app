3ae4bbab8e7ad54bc225c0c2fde74ce6f9ed1a43
Matias Ramos <matiasramos@gmail.com>
2023-03-29 00:25:46 -0300

    fix url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 export const API_URL = import.meta.env.MODE === 'development' ?
     'http://localhost:8010/proxy/api/v1' :
-    'https://profiles-5q3a.onrender.com'
+    'https://profiles-5q3a.onrender.com/api/v1'
 
 export const DEFAULT_AVATAR = 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659397105/profiles-app/default-avatar_tubdyf.jpg'
