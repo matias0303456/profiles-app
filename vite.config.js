42f58cc749c5d835365f285222e8c594fbf7f154
Matias Ramos <matiasramos@gmail.com>
2023-08-03 13:54:29 -0300

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/profiles-app/',
+  base: '/profiles/',
   build: {
     outDir: './docs'
   }
