a194bcfdfd64bf015875acd190ab6f03cc80020f
Matias Ramos <matiasramos@gmail.com>
2023-08-02 22:58:21 -0300

    deploy

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,3 @@
-export const API_URL = import.meta.env.MODE === 'development' ?
-    'http://localhost:8010/proxy/api/v1' :
-    'https://profiles-5q3a.onrender.com/api/v1'
+export const API_URL = 'http://vxsct2618.avnam.net:8000/'
 
 export const DEFAULT_AVATAR = 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659397105/profiles-app/default-avatar_tubdyf.jpg'
