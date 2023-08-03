943784e897d2adcecf728bd9ceb0757c46c00d8c
Matias Ramos <matiasramos@gmail.com>
2023-08-02 23:25:44 -0300

    build

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://vxsct2618.avnam.net:8000/api/v1'
+export const API_URL = 'vxsct2618.avnam.net:8000/api/v1'
 
 export const DEFAULT_AVATAR = 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659397105/profiles-app/default-avatar_tubdyf.jpg'
