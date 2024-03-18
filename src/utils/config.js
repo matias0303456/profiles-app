7bf5de4d91f32db68f2d5455282778bc2b6c785d
Matias Ramos <matias.ramos@newbrick.co>
2024-03-18 12:31:49 -0300

    deploy 18-3

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://vxsct2618.avnam.net:8000/api/v1'
+export const API_URL = 'http://localhost:9400/api/v1'
 
 export const DEFAULT_AVATAR = 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659397105/profiles-app/default-avatar_tubdyf.jpg'
