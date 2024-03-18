0ba18ed15fea3dec27be95d85b94e2e6fdd657e6
Matias Ramos <matias.ramos@newbrick.co>
2024-03-18 12:48:27 -0300

    deploy 18-3 2

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:9400/api/v1'
+export const API_URL = 'https://vps-3895181-x.dattaweb.com/profiles/api/v1'
 
 export const DEFAULT_AVATAR = 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659397105/profiles-app/default-avatar_tubdyf.jpg'
